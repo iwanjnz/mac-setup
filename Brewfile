eb4075c835a8cb344bfa753f7ad99530ac09246b
Iwan Jones <igjones.55@gmail.com>
2018-12-20 12:33:08 +0000

    Initial commit, working version

---
+++ Brewfile
@@ -0,0 +1,18 @@
+tap "homebrew/bundle"
+tap "caskroom/cask"
+brew "mas"
+cask "google-chrome"
+cask "sonos"
+cask "disk-inventory-x"
+cask "spotify"
+cask "transmission"
+cask "atom"
+cask "the-unarchiver"
+cask "dropbox"
+cask "malwarebytes"
+cask "parallels"
+cask "virtualbox"
+mas "Bear", id: 1091189122
+mas "LastPass Password Manager", id: 926036361
+mas "Pocket", id: 568494494
+mas "Lynda.com", id: 985958984
---
+++ installTest.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#testing if the brew and git scripts install hombrew and git
+#should reject if already installed ... this works
+
+
+if ! command -v brew > /dev/null 2>&1; then
+  echo "Installing homebrew"
+  yes | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+
+  echo "Homebrew already installed"
+
+fi
+
+if ! command -v git > /dev/null 2>&1; then
+  echo "Installing git"
+  yes | brew install git
+
+else
+
+  echo "Git already installed"
+
+fi
+
+if ! command -v bundle > /dev/null 2>&1; then
+  echo "Installing bundle"
+  yes | brew tap Homebrew/bundle
+
+else
+
+  echo "bundle already installed"
+
+fi
+
+#brew tap Homebrew/bundle
+
+#wd
+brew tap caskroom/cask
+
+cd Desktop/install
+
+cp sonos.rb /usr/local/homebrew/Library/Taps/homebrew/homebrew-cask/Casks
+
+brew bundle
+
+zsh
